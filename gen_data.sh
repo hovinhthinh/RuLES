fa128a59aeed7a7fc1d89d08fa6991a0632f7029
Vinh Thinh Ho <hovinhthinh@gmail.com>
2018-03-25 11:18:46 +0200

    gen global

--- gen_data.sh
+++ gen_data.sh
@@ -2,7 +2,7 @@
 
 echo 'Separating ideal data into training and test data'
 javac util/TrainingGen.java
-java -classpath util/ TrainingGen $1 $2 true
+java -classpath util/ TrainingGen $1 $2 ${3:-true}
 rm util/TrainingGen.class
 echo 'Normalizing data'
 python util/gen.py $1
