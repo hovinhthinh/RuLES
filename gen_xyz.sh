367c5462d57c48b9d034c48217514a2fe813da02
Vinh Thinh Ho <hvthinh@sedna.mpi-inf.mpg.de>
2018-03-25 18:34:18 +0200

    fix

--- gen_xyz.sh
+++ gen_xyz.sh
@@ -1,9 +1,7 @@
 # args: <workspace>
 python util/encode_input_rumis.py "$1/training.data.txt" > tmp
 java -jar comparison/rumis-1.0.jar -e=pos -l=tmp
-cat horn-rules-stats.txt | awk -F$'\t' '$2 >= 10' > "$1/xyz.tmp"
-rm horn-rules.txt
-rm horn-rules-stats.txt
-rm tmp
-java -cp mining/build.jar de.mpii.util.GenXYZ "$1" "$1/xyz.tmp" "$1/xyz.input"
-rm "$1/xyz.tmp"
+cat horn-rules-stats.txt | awk -F$'\t' '$2 >= 10' > tmp1
+java -cp mining/build.jar de.mpii.util.GenXYZ "$1" tmp1 tmp2
+cat tmp2 | awk -F$'\t' '$2 >= 10' > "$1/xyz.input"
+rm horn-rules.txt horn-rules-stats.txt tmp tmp1 tmp2
