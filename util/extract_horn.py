b43009b27b011ef80a31330194969109e58149f8
Vinh Thinh Ho <hvthinh@sedna.mpi-inf.mpg.de>
2018-03-29 17:28:38 +0200

    fix

--- util/extract_horn.py
+++ util/extract_horn.py
@@ -3,7 +3,8 @@ import sys
 # sys.argv = ["$", "./exp3/xyzna4nv3nna1mc.1hc.0cms10ew.3.sorted", "0.2", "./exp3/fb15k.embed.10.ec02"]
 # create embedding training dataset for imdb.
 input = sys.argv[1]
-mec = float(sys.argv[2])
+msup = int(sys.argv[2])
+mec = float(sys.argv[3])
 
 horn_to_line = {}
 
@@ -15,6 +16,9 @@ with open(input) as f:
         if not_pos == -1:
             continue
         ec = float(line.split('\t')[12])
+	sup = int(line.split('\t')[10])
+	if sup < msup:
+            continue
         if ec < mec:
             continue
         horn = line[0:not_pos]
@@ -23,6 +27,6 @@ with open(input) as f:
         horn_to_line[horn] = line
         out.append(line)
 
-with open(sys.argv[3], "w") as f:
+with open(sys.argv[4], "w") as f:
     for line in out:
         f.write(line + "\n")
